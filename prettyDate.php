53844bd631ca76222e28e7e73c80bc80bc02031d
kukko <nyariag@gmail.com>
2015-03-31 20:55:41 +0200

    Simplify localization
    Putted all the variables into an array, which values is language
    specific.

--- prettyDate.php
+++ prettyDate.php
@@ -1,11 +1,33 @@
 <?php
-	function szepDatum($input, $mod="short", $day=false){
-		$months=array("Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December");
-		$monthsShort=array("Jan", "Feb", "Már", "Ápr", "Máj", "Jún", "Júl", "Aug", "Szep", "Okt", "Nov", "Dec");
+	function szepDatum($input, $mod="short", $day=false, $lang="HU"){
+		$localize=array(
+			"today"=>array(
+				"HU"=>"Ma ",
+				"EN"=>"Today "
+			),
+			"months"=>array(
+				"long"=>array(
+					"HU"=>array(
+						"Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December")
+						),
+					"EN"=>array(
+						"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+					)
+				),
+				"short"=>array(
+					"HU"=>array(
+						"Jan", "Feb", "Már", "Ápr", "Máj", "Jún", "Júl", "Aug", "Szep", "Okt", "Nov", "Dec"
+					),
+					"EN"=>array(
+						"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+					)
+				)
+			)
+		);
 		if (substr($input, 0, 10)==date("Y-m-d")){
 			$output="";
 			if ($day=true){
-				$output="Ma ";
+				$output=$localize["today"][$lang];
 			}
 			$output.=substr($input, 11, 5);
 		}
@@ -14,10 +36,10 @@
 				$date=substr($input, 5, 5);
 				list($month, $day)=explode("-", $date);
 				if ($mod=="long" || $mod=="superlong"){
-					$month=$months[$month-1];
+					$month=$localize["months"]["long"][$lang][$month-1];
 				}
 				if ($mod=="short"){
-					$month=$monthsShort[$month-1];
+					$month=$localize["months"]["short"][$lang][$month-1];
 				}
 				$output=$month." ".$day.".";
 			}
@@ -25,10 +47,10 @@
 				$date=substr($input, 0, 10);
 				list($year, $month, $day)=explode("-", $date);
 				if ($mod=="long" || $mod=="superlong"){
-					$month=$months[$month-1];
+					$month=$localize["months"]["long"][$lang][$month-1];
 				}
 				if ($mod=="short"){
-					$month=$monthsShort[$month-1];
+					$month=$localize["months"]["short"][$lang][$month-1];
 				}
 				if ($mod=="superlong"){
 					$output=$year." ".$month." ".$day.".";
